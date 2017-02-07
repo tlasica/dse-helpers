b8820ecb58c4dba6fc31dbc12f7e669410b7cbd3
Tomasz Lasica <tomasz.lasica@datastax.com>
2017-02-07 12:06:31 -0500

    Find DSE by pid

--- bin/dumpjfr.sh
+++ bin/dumpjfr.sh
@@ -3,7 +3,12 @@ FILENAME=$1
 DELAY=$2
 DURATION=$3
 JCMD=`which jcmd`
-DSE_PID=`$JCMD | grep DseModule | cut -c1,2,3,4,5`
+DSE_PID=`ps aux | grep dse.server_process | grep -v "grep" | cut -c10,11,12,13,14,15`
+if [ "x$DSE_PID" == "x" ]
+then
+    DSE_PID=`$JCMD | grep DseModule | cut -c1,2,3,4,5`
+fi
+
 $JCMD $DSE_PID VM.unlock_commercial_features
-$JCMD $DSE_PID JFR.start name=dse settings=profile filename=$FILENAME duration=$DURATION delay=$DELAY
+$JCMD $DSE_PID JFR.start name=dse settings=profile settings=memory filename=$FILENAME duration=$DURATION delay=$DELAY
 
