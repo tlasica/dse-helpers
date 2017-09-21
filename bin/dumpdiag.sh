80d57a05d24f00f11b2157388c5afc71f4ef5072
Tomek Lasica <tomasz.lasica@datastax.com>
2017-09-21 10:40:54 +0200

    Add dumpdiag.sh for essential diagnostics info

---
+++ bin/dumpdiag.sh
@@ -0,0 +1,42 @@
+# Dumps important DSE diagnostics such as: logs, jfr, conf and threaddump
+# into 4 files: conf.tar.gz, logs.tar.gz, threaddump.txt and dse.jfr
+# into a directory specified as $1
+
+if [ -z "$1" ]
+then
+    echo "Usage: dumpdiag.sh dir"
+    echo
+    exit 1
+fi
+
+OUTDIR=$1
+
+mkdir -p $OUTDIR
+
+echo "..dumping configuration dse/resources"
+tar czf $OUTDIR/conf.tar.gz `find dse/resources -type f | grep  '/conf/'`
+
+echo "..dumping logs"
+tar czf $OUTDIR/logs.tar.gz /var/log/cassandra
+
+JCMD=`which jcmd`
+DSE_PID=`ps aux | grep dse.server_process | grep -v "grep" | cut -c10,11,12,13,14,15`
+if [ "x$DSE_PID" == "x" ] 
+then
+    DSE_PID=`$JCMD | grep DseModule | cut -c1,2,3,4,5`
+fi
+
+if [ "x$DSE_PID" == "x" ] 
+then
+	echo "..DSE process not found, skipping threadump and jfr"
+else
+	echo "..DSE process found: $DSE_PID, requesting jfr"
+	$JCMD $DSE_PID VM.unlock_commercial_features
+	$JCMD $DSE_PID JFR.start name=dse settings=profile filename=$OUTDIR/dse.jfr duration=60s delay=10s
+	echo "..generating thread dump for DSE: $DSE_PID"
+	jstack -l $DSE_PID > $OUTDIR/threaddump.txt
+	echo "..watiting 70s for JFR to finish"
+	sleep 70
+fi
+
+ls $OUTDIR
